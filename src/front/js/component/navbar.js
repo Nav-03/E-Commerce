019f103ef36345e5d2e0bb0157efac57a87ab7f0
Nav-03 <navil.gabrie@gmail.com>
2022-04-29 21:57:13 +0000

    added registration and links,installed bootstrap

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -12,7 +12,7 @@ export const Navbar = () => {
 				<div className="collapse navbar-collapse" id="navbarSupportedContent">
 					<ul className="navbar-nav me-auto mb-2 mb-lg-0">
 						<li className="nav-item">
-							<a className="nav-link active" aria-current="page" href="#">Register</a>
+							<a className="nav-link active" aria-current="page" href="/registration">Register</a>
 						</li>
 						<li className="nav-item">
 							<a className="nav-link" href="#">Link</a>
@@ -22,14 +22,14 @@ export const Navbar = () => {
 								Dropdown
 							</a>
 							<ul className="dropdown-menu" aria-labelledby="navbarDropdown">
-								<li><a className="dropdown-item" href="/Demo">Demo</a></li>
+								<li><a className="dropdown-item" href="/demo">Demo</a></li>
 								<li><a className="dropdown-item" href="/single">Single</a></li>
 								<li><hr className="dropdown-divider" /></li>
-								<li><a className="dropdown-item" href="#">Something else here</a></li>
+								<li><a className="dropdown-item" href="/registration">Registration</a></li>
 							</ul>
 						</li>
 						<li className="nav-item">
-							<a className="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+							<a className="nav-link disabled" href="#" tabIndex="-1" aria-disabled="true">Disabled</a>
 						</li>
 					</ul>
 					<form className="d-flex">
