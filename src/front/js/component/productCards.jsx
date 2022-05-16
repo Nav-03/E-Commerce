53ee70f61ff8cd7808ed9f0c430851da1b192575
Nav-03 <navil.gabrie@gmail.com>
2022-05-16 21:15:21 +0000

    added photos and further styling

--- src/front/js/component/productCards.jsx
+++ src/front/js/component/productCards.jsx
@@ -8,32 +8,79 @@ import Fade from "react-reveal/Fade";
 export const ProductCard = () => {
   return (
     <>
-      <Row xs={1} md={3} className="g-4">
-        <br />
+      <div className="d-flex flex-row">
         <Fade bottom>
-          {Array.from({ length: 9 }).map((_, idx) => (
-            <Col className="">
-              <Card className="shadow bg-body rounded">
-                <Card.Img
-                  variant="top"
-                  src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22402%22%20height%3D%22160%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20402%20160%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1807772f2d4%20text%20%7B%20fill%3A%23999%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A20pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1807772f2d4%22%3E%3Crect%20width%3D%22402%22%20height%3D%22160%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22147.82421875%22%20y%3D%2289.45%22%3E402x160%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
-                />
-                <Card.Body>
-                  <Card.Title>Card title</Card.Title>
-                  <Card.Text>
-                    This is a longer card with supporting text below as a
-                    natural lead-in to additional content. This content is a
-                    little bit longer.
-                  </Card.Text>
-                  <Button variant="primary" size="sm">
-                    Learn more
-                  </Button>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))}
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src="https://www.autozone.com/cdn/images/B2C/US/media/FY20/P05/MobileFirst/fy20p0506-2split9col-ocs-d.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src="https://www.autozone.com/cdn/images/B2C/US/media/FY22/P07/BrakeAssets/2split-brakeJob-lockup-15-d.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
         </Fade>
-      </Row>
+      </div>
+      <div>
+        <Fade>
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src="https://www.autozone.com/cdn/images/B2C/US/media/FY22/P10/SplitBlocks/_fy22p10-2s-ac-running-low-d.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </Fade>
+      </div>
+      <div>
+        <Fade>
+          <Card className="bg-dark text-white">
+            <Card.Img
+              src="https://www.autozone.com/cdn/images/B2C/US/media/FY22/P10/Catbans/fy22p10-catban12-battery-destination-d.jpg"
+              alt="Card image"
+            />
+            <Card.ImgOverlay>
+              <Card.Title>Card title</Card.Title>
+              <Card.Text>
+                This is a wider card with supporting text below as a natural
+                lead-in to additional content. This content is a little bit
+                longer.
+              </Card.Text>
+              <Card.Text>Last updated 3 mins ago</Card.Text>
+            </Card.ImgOverlay>
+          </Card>
+        </Fade>
+      </div>
     </>
   );
 };
--- src/front/js/component/productCarousel.jsx
+++ src/front/js/component/productCarousel.jsx
@@ -9,7 +9,7 @@ export const ProductCarousel = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1807764b7e5%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1807764b7e5%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23373940%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22292.81640625%22%20y%3D%22218.75%22%3EFirst%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            src="https://www.autozone.com/cdn/images/B2C/US/media/FY22/P10/Heros/fy22p10-superhero-battery-destination-d.jpg"
             alt="First slide"
           />
           <Carousel.Caption>
@@ -20,7 +20,7 @@ export const ProductCarousel = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1807764b7e6%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1807764b7e6%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%23282c34%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22255.0859375%22%20y%3D%22218.75%22%3ESecond%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            src="https://www.autozone.com/cdn/images/B2C/US/media/FY22/P10/Heros/fy22p10-superhero-complete-brakejob15%-d.jpg"
             alt="Second slide"
           />
 
@@ -32,7 +32,7 @@ export const ProductCarousel = () => {
         <Carousel.Item>
           <img
             className="d-block w-100"
-            src="data:image/svg+xml;charset=UTF-8,%3Csvg%20width%3D%22800%22%20height%3D%22400%22%20xmlns%3D%22http%3A%2F%2Fwww.w3.org%2F2000%2Fsvg%22%20viewBox%3D%220%200%20800%20400%22%20preserveAspectRatio%3D%22none%22%3E%3Cdefs%3E%3Cstyle%20type%3D%22text%2Fcss%22%3E%23holder_1807764b7e6%20text%20%7B%20fill%3A%23ffffff%3Bfont-weight%3Anormal%3Bfont-family%3Avar(--bs-font-sans-serif)%2C%20monospace%3Bfont-size%3A40pt%20%7D%20%3C%2Fstyle%3E%3C%2Fdefs%3E%3Cg%20id%3D%22holder_1807764b7e6%22%3E%3Crect%20width%3D%22800%22%20height%3D%22400%22%20fill%3D%22%2320232a%22%3E%3C%2Frect%3E%3Cg%3E%3Ctext%20x%3D%22283.3515625%22%20y%3D%22218.75%22%3EThird%20slide%3C%2Ftext%3E%3C%2Fg%3E%3C%2Fg%3E%3C%2Fsvg%3E"
+            src="https://www.autozone.com/cdn/images/B2C/US/media/FY22/P10/Heros/_fy22p10-superhero-lucas-3-d.jpg"
             alt="Third slide"
           />
 
