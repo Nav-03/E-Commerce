1ab7f660bdb7d9408df8417273ef36bd93dc0688
Nav-03 <navil.gabrie@gmail.com>
2022-05-17 01:34:50 +0000

    added partial back end

--- src/api/routes.py
+++ src/api/routes.py
@@ -4,15 +4,40 @@ This module takes care of starting the API Server, Loading the DB and Adding the
 from flask import Flask, request, jsonify, url_for, Blueprint
 from api.models import db, User
 from api.utils import generate_sitemap, APIException
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
 api = Blueprint('api', __name__)
 
 
-@api.route('/hello', methods=['POST', 'GET'])
-def handle_hello():
+# @api.route('/hello', methods=['POST', 'GET'])
+# def handle_hello():
 
-    response_body = {
-        "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
-    }
+#     response_body = {
+#         "message": "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
+#     }
 
-    return jsonify(response_body), 200
+#     return jsonify(response_body), 200
+
+
+
+@api.route('/user', methods=['POST'])
+def create_user():
+    user = request.get_json()
+    if request.json is None:
+        return jsonify({"msg":"Missing the payload"}), 400
+    name = request.json.get('name', None)
+    email = request.json.get('email', None)
+    password = request.json.get('password', None)
+    user = User(name=name, email=email, password=password)
+    db.session.add(user)
+    db.session.commit()
+    return jsonify(user.serialize())
+
+
+@api.route('/user', methods=['GET'])
+@jwt_required()
+def user():
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+
+    return jsonify({"id": user.id, "email": user.email }), 200
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,6 +1,10 @@
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
+			user: [],
+
+
+
 			message: null,
 			demo: [
 				{
@@ -41,7 +45,71 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//reset the global store
 				setStore({ demo: demo });
-			}
+			},
+			getCurrentSession: () => {
+				const session = JSON.parse(localStorage.getItem("session"));
+				setStore({ session });
+				return session;
+			},
+			createNewSession: async (email, password) => {
+				const options = {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({ email: email, password: password }),
+				};
+				const response = await fetch(
+					process.env.BACKEND_URL + `/api/token`,
+					options
+				);
+				if (!response.ok)
+					throw Error("There was a problem in the login request");
+				if (response.status === 401) {
+					throw "Invalid credentials";
+				} else if (response.status === 400) {
+					throw "Invalid email or password format";
+				}
+				if (response.status === 200) {
+					const payload = await response.json();
+					localStorage.setItem("session", JSON.stringify(payload));
+					setStore({ session: payload });
+					return payload; //this is gonna make the promise resolve
+					// return jsonify({ "token": access_token, "user_id": user.id })
+				} else {
+					return await response.json();
+				}
+			},
+			addUser: async (name, email, id, password) => {
+				const options = {
+					method: "POST",
+					headers: { "Content-Type": "application/json" },
+					body: JSON.stringify({
+						name: name,
+						email: email,
+						id: id,
+						password: password
+					}),
+				};
+				const response = await fetch(
+					process.env.BACKEND_URL + `/api/user/${id}`,
+					options
+				);
+				if (response.status === 200) {
+					const payload = await response.json();
+					setStore({ user: payload });
+					return payload;
+				}
+			},
+			loadUser: async (id) => {
+				const response = await fetch(
+					process.env.BACKEND_URL + `/api/user/${id}`
+				);
+				if (response.status === 200) {
+					const payload = await response.json();
+					// setStore({ guest: payload });
+					return payload;
+				}
+			},
+
 		}
 	};
 };
